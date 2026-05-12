094cf20cd6d7a701a4a0b0acd9473a50ded1ddd1
Tran Le Anh Qui <anhqui04062004@gmail.com>
2026-05-12 12:25:50 +0700

    delete cms

--- EcommerWebCar/cms-service/src/main/java/com/tlaq/cms_service/repo/NewsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tlaq.cms_service.repo;
-
-import com.tlaq.cms_service.entity.News;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NewsRepository extends JpaRepository<News, Long> {
-}
